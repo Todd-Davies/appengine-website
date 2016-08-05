ca12f53952fb711f6c9be57ec123783283cf6e4a
Todd Davies <todd434@gmail.com>
2016-08-05 18:00:20 +0100

    Cleaned up the contact encryption logic

--- src/main/webapp/js/contact.js
+++ src/main/webapp/js/contact.js
@@ -42,42 +42,19 @@ var submitAnswer = function() {
  * N.b. Yes, I know this isn"t secure, but it's primary purpose is obfuscation rather than encryption
  */
 var xor = function(mode, password, input) {
-        if(password.length==0||input.length==0) return ""; //If there was no input
-        password = password.toLowerCase();
-        if(mode) {
-                if(password.length<(input.length)) {
-                        var count = 0;
-                        while(password.length<(input.length)) {
-                                password += password.charAt(count);
-                                count++;
-                                if(count==password.length) {
-                                        count = 0;
-                                }
-                        }
-                }
-        } else {
-                if(password.length<(input.length/4)) {
-                        var count = 0;
-                        while(password.length<(input.length/4)) {
-                                password += password.charAt(count);
-                                count++;
-                                if(count==password.length) {
-                                        count = 0;
-                                }
-                        }
-                }
-        }
-        var outputString = "";
-        var cipherChars = input.split("-");
-        for (var i=0;i<password.length;i++) {
-                if(mode) {
-                        var toAdd = (password.charCodeAt(i) ^ input.charCodeAt(i));
-                        outputString += ((i>0) ? "-" : "") + ((toAdd<100) ? ((toAdd<10) ? toAdd + "XX" : toAdd + "X") : toAdd); //To Encrypt
-                } else {
-                        var thisChar = cipherChars[i];
-                        thisChar = thisChar.replace("X", "");
-                        outputString += String.fromCharCode(password.charCodeAt(i) ^ parseInt(thisChar, 10));
-                }
-        }
-        return outputString;
+  if(password.length==0||input.length==0) return ""; //If there was no input
+  password = password.toLowerCase();
+  if(password.length<(input.length/4)) {
+    for (var i = 0; password.length < input.length / 4; i++) {
+      password += password.charAt(i % password.length);
+    }
+  }
+  var outputString = "";
+  var cipherChars = input.split("-");
+  for (var i=0;i<password.length;i++) {
+    var thisChar = cipherChars[i];
+    thisChar = thisChar.replace("X", "");
+    outputString += String.fromCharCode(password.charCodeAt(i) ^ parseInt(thisChar, 10));
+  }
+  return outputString;
 }
