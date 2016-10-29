6af70755265c9013fee0f5843a58431747092072
Todd Davies <todd434@gmail.com>
2016-10-29 18:22:35 +0100

    Fixed untracked files

--- src/main/java/uk/co/todddavies/website/cron/tasks/HabiticaApiKeys.java
+++ src/main/java/uk/co/todddavies/website/cron/tasks/HabiticaApiKeys.java
@@ -4,6 +4,6 @@ public final class HabiticaApiKeys {
   
   private HabiticaApiKeys() {/* Private constructor */}
   
-  static String USER = "";
-  static String KEY = "";
+  static String USER = "da6bb7be-ed3a-49d1-8496-12e075310ddd";
+  static String KEY = "d9deadb3-588b-474b-8339-ea469869114c";
 }
