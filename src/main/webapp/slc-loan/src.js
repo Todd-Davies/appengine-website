b94bb0c51cc1e69d795aa64e4619f5c28a9cf63d
Todd Davies <todd434@gmail.com>
2017-12-30 00:13:55 +0100

    Made the slc calculator better

--- src/main/webapp/slc-loan/src.js
+++ src/main/webapp/slc-loan/src.js
@@ -1,4 +1,4 @@
-var compute = function(oneoff, loan, plan, income, p1interest) {
+var compute = function(oneoff, loan, plan, income, p1interest, gradyear) {
   const plan1 = plan == "p1"
   var interest = 0;
   if (plan1) {
@@ -9,19 +9,25 @@ var compute = function(oneoff, loan, plan, income, p1interest) {
     interest = rpi + added;
   }
 
-  // Only charged after 21k
+  const writeoffyears = plan1 ? 25 : 30;
+
+  // Only charged after 17.5k/21k
   const non_taxable = plan1 ? 17500 : 21000;
   // The percentage of salary that is deducted
   const tax_amount = 9.0;
 
   var total_contribution = oneoff;
   var total_interest = 0;
-  var year = 2018; // TODO: update this
+  var year = gradyear;
+  console.log(gradyear)
 
   loan -= oneoff;
 
   const monthly_payment = Math.floor((((income - non_taxable) / 100.0) * tax_amount) / 12.0);
-  while (loan > 0 && year <= (2018 + 30)) {
+  if (monthly_payment <= 0) {
+    monthly_payment = 0;
+  }
+  while (loan > 0 && year <= (gradyear + writeoffyears)) {
     // Simulate every month
     for (var i = 0; i < 12; i++) {
       const interest_amount = (loan / 100.0 * interest) / 12.0;
@@ -45,10 +51,10 @@ var compute = function(oneoff, loan, plan, income, p1interest) {
   return [total_interest, year, total_contribution, loan].map(function(i) { return parseInt(i); });
 }
 
-var refreshData = function(loan, plan, p1interest, income) {
+var refreshData = function(loan, plan, p1interest, income, gradyear) {
   var data = []
   for (var oneoff = 0; oneoff <= loan; oneoff += 500) {
-    const x = compute(oneoff,  loan, plan, income, p1interest);
+    const x = compute(oneoff,  loan, plan, income, p1interest, gradyear);
     data.push([oneoff, x[0], x[1], x[2], x[3]]);
   }
   return data;
