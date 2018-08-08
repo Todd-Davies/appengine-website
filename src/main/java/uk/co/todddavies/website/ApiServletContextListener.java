13f0086d14bed6b342f1473840a06627c7594123
Todd Davies <todd434@gmail.com>
2018-08-08 23:47:21 +0200

    Removed the old code for the contact page.

--- src/main/java/uk/co/todddavies/website/ApiServletContextListener.java
+++ src/main/java/uk/co/todddavies/website/ApiServletContextListener.java
@@ -1,17 +1,13 @@
 package uk.co.todddavies.website;
 
-import uk.co.todddavies.website.cache.MemcacheModule;
-import uk.co.todddavies.website.closure.TemplateServletModule;
-import uk.co.todddavies.website.contact.ContactServletModule;
-import uk.co.todddavies.website.cron.tasks.CronTasksServletModule;
-import uk.co.todddavies.website.notes.NotesServletModule;
-import uk.co.todddavies.website.notes.data.NotesDatastoreModule;
-import uk.co.todddavies.website.pages.PagesServletModule;
-import uk.co.todddavies.website.sslverification.SslVerificationServletModule;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
+import uk.co.todddavies.website.cache.MemcacheModule;
+import uk.co.todddavies.website.closure.TemplateServletModule;
+import uk.co.todddavies.website.notes.NotesServletModule;
+import uk.co.todddavies.website.notes.data.NotesDatastoreModule;
+import uk.co.todddavies.website.sslverification.SslVerificationServletModule;
 import uk.co.todddavies.website.taskqueue.TaskQueueModule;
 
 public final class ApiServletContextListener  extends GuiceServletContextListener {
@@ -22,10 +18,8 @@ public final class ApiServletContextListener  extends GuiceServletContextListene
     return Guice.createInjector(
         RedirectModule.create(),
         new JsonObjectWriterModule(),
-        new PagesServletModule(API_PATH),
         // Used when verifying ownership of the domain for https renewal
         new SslVerificationServletModule(false),
-        ContactServletModule.create(API_PATH),
         NotesServletModule.create(API_PATH),
         TemplateServletModule.create(),
         // Global notes binding modules to avoid multiple bindings
