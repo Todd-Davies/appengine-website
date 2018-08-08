240cfa2c8fcaaf4d34c264f1ce88e5beb43c755e
Todd Davies <todd434@gmail.com>
2018-08-08 23:50:36 +0200

    Deleted an obsolete module for managing credentials.

--- src/main/java/uk/co/todddavies/website/credentials/Credentials.java
+++
@@ -1,9 +0,0 @@
-package uk.co.todddavies.website.credentials;
-
-public final class Credentials {
-  // These are revoked, we're all good.
-  public static final long HABITICA_KEY = 5762637883244544L;
-  public static final long HABITICA_USER = 5741031244955648L;
-
-  private Credentials() {/* Private constructor */ }
-}
