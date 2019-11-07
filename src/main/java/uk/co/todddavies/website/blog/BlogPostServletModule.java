3e23bbbd4bfe2385267e138e037130f3fde6a022
Todd Davies <todd434@gmail.com>
2019-11-07 19:55:17 +0100

    Write a post on the conservative party going into the 2019 general election.

--- src/main/java/uk/co/todddavies/website/blog/BlogPostServletModule.java
+++ src/main/java/uk/co/todddavies/website/blog/BlogPostServletModule.java
@@ -13,7 +13,8 @@ import java.io.IOException;
 @Singleton
 final class BlogPostServletModule extends HttpServlet {
   static final ImmutableMap<String, String> PATH_MAP = ImmutableMap.of(
-          "uk-election-2019-series-intro", "ukElection19SeriesIntro",
+      "uk-election-2019-series-conservatives", "ukElection19SeriesConservatives",
+      "uk-election-2019-series-intro", "ukElection19SeriesIntro",
       "getting-stuff-down", "gettingStuffDown",
       "how-to-pull", "howToPull",
       "starting-a-blog", "startingABlog"
